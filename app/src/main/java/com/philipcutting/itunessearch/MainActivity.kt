c4aaa06a34f6fc70b3f11b948d3766be638c4c87
Phil Cutting <phil@nevie.com>
2021-04-30 21:20:14 +0300

    assigning data from web api to models.

--- app/src/main/java/com/philipcutting/itunessearch/MainActivity.kt
+++ app/src/main/java/com/philipcutting/itunessearch/MainActivity.kt
@@ -3,10 +3,13 @@ package com.philipcutting.itunessearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.philipcutting.itunessearch.models.StoreItem
+import com.philipcutting.itunessearch.models.StoreResultsList
 import com.philipcutting.itunessearch.utils.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import java.net.URL
 
 /*
@@ -15,24 +18,31 @@ Use the run extension function to set up the request, connect to the endpoint an
 Once the data has been retrieved, print it to the console.
  */
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var queryMap : Map<String,String> = mapOf(
+        val queryMap : Map<String,String> = mapOf(
             "term" to "red+velvet",
-            "media" to "music",
+            "media" to "musicq",
             "limit" to "500"
         )
 
-        val base = "https://itunes.apple.com/search?"
-        var url = URL(base)
-        url = url.buildUrl(queryMap)
-        CoroutineScope(Dispatchers.IO).launch {
-                Log.i(TAG,"getItunesJson:  ${getItunesJson(url) ?: "NULL"}")
+        fetchItems(queryMap){
+            it?.forEach { item ->
+                Log.d(TAG, "'${item?.trackName}' by '${item?.artistName}'")
             }
+
+        }
+
+
+
+
+
     }
 }
--- app/src/main/java/com/philipcutting/itunessearch/utils/Utilities.kt
+++ app/src/main/java/com/philipcutting/itunessearch/utils/Utilities.kt
@@ -1,11 +1,18 @@
 package com.philipcutting.itunessearch.utils
 
 import android.util.Log
+import com.philipcutting.itunessearch.models.StoreItem
+import com.philipcutting.itunessearch.models.StoreResultsList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.jetbrains.annotations.NotNull
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.URL
+import java.util.*
 import javax.net.ssl.HttpsURLConnection
 
 const val TAG = "Utilities.kt"
@@ -27,6 +34,37 @@ fun URL.buildUrl(queryMap: Map<String,String>):URL {
     val url = URL(uri)
     Log.i(TAG, " \"URL -> ${url}, path -> ${url}, file -> ${url.file},query -> ${url.query}")
     return url
+}
+
+fun fetchItems(query: Map<String,String>, onComplete: (MutableList<StoreItem?>?) -> Unit) {
+    val permittedQueryKeys = listOf(
+        "term", "country", "media", "entity", "attribute",
+        "callback", "limit", "lang", "version", "explicit"
+    )
+
+    val validQuery = mutableMapOf<String, String>()
+    query.forEach { (key, value) ->
+        if (!permittedQueryKeys.contains(key) && value != "") {
+            Log.e(TAG, "Invalid search parameter passed to search query: $key")
+        } else {
+            validQuery[key] = value
+        }
+    }
+
+    val base = "https://itunes.apple.com/search?"
+    var url = URL(base)
+    url = url.buildUrl(validQuery)
+    var data: String?
+    CoroutineScope(Dispatchers.IO).launch {
+        data = getItunesJson(url)
+        try {
+            val json = JSONObject(data)
+            val store = StoreResultsList(json)
+            onComplete(store.results)
+        } catch (ex: java.lang.Exception) {
+            Log.e(TAG, "unable to perform search, check your search criteria: Error $ex")
+        }
+    }
 }
 
 private fun streamToString(inputStream: InputStream): String {
